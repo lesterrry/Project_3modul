8285d77962868e7cbb02018d29897200398a68fe
Yana Glukhova <yana.glukhoba@gmail.com>
2025-03-03 00:37:03 +0300

    Initial Commit

---
+++ Project_3modul/Project_3modulApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3modulApp.swift
+//  Project_3modul
+//
+//  Created by MacBook - Latter on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Project_3modulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
